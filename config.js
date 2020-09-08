662110895c7ba0b519c2f05326b62090bca7f43e
rakeshcocoalabs <rakesh@cocoalabs.in>
2020-09-08 15:20:59 +0530

    creating new gateway

---
+++ config.js
@@ -0,0 +1,44 @@
+module.exports = {
+	port:4000,
+	routes:  [
+		{
+			path:"address",
+			port:"4001"
+		},
+		{
+			path:"admin",
+			port:"4002"
+		},
+		{
+			path:"categories",
+			port:"4003"
+		},
+		{
+			path:"delivery",
+			port:"4004"
+		},
+		{
+			path:"order",
+			port:"4005"
+		},
+		{
+			path:"product",
+			port:"4006"
+		},
+		{
+			path:"promocode",
+			port:"4007"
+		},
+		{
+			path:"user",
+			port:"4008"
+		},
+		{
+			path:"vendor",
+			port:"4009"
+		}
+
+
+	]
+
+}
